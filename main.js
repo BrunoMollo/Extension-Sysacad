85131c92ec0362697e9428c5eced5b77383be417
Bruno <you@example.com>
2021-12-01 15:21:20 -0300

    movi el codigo que importa a el css al mainLoader;

--- main.js
+++ main.js
@@ -2,25 +2,7 @@ import Materias_Todas from "./-conjuntosMaterias/Materias_Todas.js";
 
 //Tengo que hacer una fucnion principal y expostral para poder  modularizar el codigo
 export function main(){
-   
-    //Importa el css de un archivo
-    const linker=document.createElement("link");
-    linker.rel="stylesheet";
-    linker.type="text/css";
-    linker.href=chrome.extension.getURL("./style-ext.css");
-    document.body.appendChild(linker);
 
-
-    
-
-
-    //Genocidio de las materias blancas
-    /*
-    let tr_materias=document.querySelectorAll("tr .textoTabla");
-    tr_materias.forEach(tr=>{tr.hidden='true'});
-*/
-
-    //-------------------------
     let todas=new Materias_Todas();
     
     const wrap=document.querySelector(".wrapper");
